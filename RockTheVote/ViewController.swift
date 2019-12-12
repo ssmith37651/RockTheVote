d6cd88a9b96505abf111941cf861a4f45bb426c4
Stephen Smith <ssmith37651@gmail.com>
2019-12-12 08:00:34 -0500

    Initial Commit

---
+++ RockTheVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockTheVote
+//
+//  Created by Smith, Stephen Christopher on 12/12/19.
+//  Copyright © 2019 Smith, Stephen Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
